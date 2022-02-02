9e00fc43a5070abe6b97ddaa0f23c3a17a059251
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-02-02 12:42:25 +0000

    Eq and imp

--- Triangle.java
+++ Triangle.java
@@ -9,6 +9,7 @@ class Triangle {
         this.x3 = x3;
     }
 
+
     static void makeTriangle(int n){
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
@@ -19,4 +20,29 @@ class Triangle {
             }
         }
     } // the method is static because it does not need to be stateful/depends on no class and is not passed anything non-static
+
+
+    public String triangleType() {
+        if (x1 == x2 && x2 == x3) {
+            return "Equilateral";
+        }
+
+        //max
+
+        if (x1 < x2) {
+            if (x2 < x3) {
+                if (x1 + x2 < x3) return "Impossible";
+            }
+            if (x1 + x3 < x2) return "Impossible";
+        }
+
+        if (x1 < x3) {
+            if (x3 < x2) {
+                if (x1 + x3 < x2) return "Impossible";
+            }
+
+            if (x1 + x2 < x3) return "Impossible";
+        }
+    }
+
 }
