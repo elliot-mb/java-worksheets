abb22d1c057a9484a830a29dea3cd4b9e4cd50f1
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-02-02 12:30:01 +0000

    q4

---
+++ Triangle.java
@@ -0,0 +1,13 @@
+class Triangle {
+    private int x1;
+    private int x2;
+    private int x3;
+
+    Triangle(int x1, int x2, int x3) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.x3 = x3;
+    }
+
+    
+}
