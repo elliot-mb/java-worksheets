783149bbd3849e128362e7deb3ae04b03eb1143d
elliotmb <elliotbuckingham14@gmail.com>
2022-02-09 12:57:18 +0000

    fork until we talk

--- wk3/exceptions/ErrorOrResult.java
+++ wk3/exceptions/ErrorOrResult.java
@@ -2,41 +2,46 @@ import java.util.NoSuchElementException;
 
 public class ErrorOrResult {
     // public static <T> bool equal(T a, T b)
-    // equal(a, b)
-    // <Integer>equal(a, b)
-    ErrorOrResult(){
-
-    }
-
-    ErrorOrResult(){
-
-    }
-
-    static ErrorOrResult parseIntOrFail (String input){
-        try{
-            int x = Integer.parseInt(input);
-        }catch (NumberFormatException e){
-            error(e);
-        }
-        return
-    }
-    static boolean failed() {
-
-    }
-
-    static String error(NumberFormatException e) throws NoSuchElementException {
-        if (failed()) return "error message";
-        else throw new NoSuchElementException(e);
-    }
-
-    static int result() throws NoSuchElementException {
-
-    }
+    // equal<T>(a, b)
+    // equal<Integer>(a, b)
 
 
-    public static void main(String[] args) {
+// // // // DONT PUSH UNTIL MESSAGE JADE (WE'LL DECIDE WHICH VER TO USE
 
-    }
+
+//    ErrorOrResult(){
+//
+//    }
+//
+//    ErrorOrResult(){
+//
+//    }
+//
+//    static ErrorOrResult parseIntOrFail (String input){
+//        try{
+//            int x = Integer.parseInt(input);
+//        }catch (NumberFormatException e){
+//            error(e);
+//        }
+//        return
+//    }
+//    static boolean failed() {
+//
+//    }
+//
+//    static String error(NumberFormatException e) throws NoSuchElementException {
+//        if (failed()) return "error message";
+//        else throw new NoSuchElementException(e);
+//    }
+//
+//    static int result() throws NoSuchElementException {
+//
+//    }
+//
+//
+//    public static void main(String[] args) {
+//
+//    }
 }
 
     
