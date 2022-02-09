34574f8e1c8f7bc9714bab1a6317c2b0277f6b86
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-02-09 12:02:54 +0000

    changed assert from public to static

--- wk3/TestUtils.java
+++ wk3/TestUtils.java
@@ -8,25 +8,25 @@ public class TestUtils {
         if(i != j) throw new AssertionError(s);
     }
 
-    public void assertUniversalEquals(int i, int j, String e) { assertEquals(i, j, e); }
-    public void assertUniversalEquals(int i, String s, String e) { throw new AssertionError(e); }
+    static void assertUniversalEquals(int i, int j, String e) { assertEquals(i, j, e); }
+    static void assertUniversalEquals(int i, String s, String e) { throw new AssertionError(e); }
 
-    public void assertUniversalEquals(String s, int i, String e) {
+    static void assertUniversalEquals(String s, int i, String e) {
         throw new AssertionError(e);
     }
 
-    public void assertUniversalEquals(String s1, String s2, String e) {
+    static void assertUniversalEquals(String s1, String s2, String e) {
         assertEquals(s1, s2, e);
     }
 
     public static void main(String[] args) {
-        String str1 = "hello";
-        String str2 = "hello";
-        String str3 = "Hello";
-        String str4 = "mind";
-        assertEquals(str1, str2, "error: expecting " + str1 + " but was " + str2);
-        assertEquals(str2, str3, "error: expecting " + str2 + " but was " + str3);
-        assertEquals(str1, str4, "error: expecting " + str1 + " but was " + str4);
+//        String str1 = "hello";
+//        String str2 = "hello";
+//        String str3 = "Hello";
+//        String str4 = "mind";
+//        assertEquals(str1, str2, "error: expecting " + str1 + " but was " + str2);
+//        assertEquals(str2, str3, "error: expecting " + str2 + " but was " + str3);
+//        assertEquals(str1, str4, "error: expecting " + str1 + " but was " + str4);
 
         assertUniversalEquals(1, 2, "Err!"); // Err!: expecting 1(class java.lang.Integer) but got 2(class java.lang.Integer)
         assertUniversalEquals("1", "2", "Err!"); // Err!: expecting 1(class java.lang.String) but got 2(class java.lang.String)
