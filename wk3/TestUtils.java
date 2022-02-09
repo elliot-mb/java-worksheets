b1a79b06d7d4e62d0e399c14bc1ef9988d6c7027
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-02-09 11:43:49 +0000

    assertEquals

--- wk3/TestUtils.java
+++ wk3/TestUtils.java
@@ -1,7 +1,27 @@
 public class TestUtils {
-    static void assertEquals(String, String, String) {return null;}
+    static void assertEquals(String s1, String s2, String s3) throws AssertionError {
+        if (!(s1.equals(s2))) throw new AssertionError(s3);
 
-    static void assertEquals(int, int, String) {return null;}
+    }
 
-    public void assertUniversalEquals();
+    static void assertEquals(int i1, int i2, String s) {}
+
+    public void assertUniversalEquals() {};
+
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = "hello";
+        String str3 = "Hello";
+        String str4 = "mind";
+        assertEquals(str1, str2, "error: expecting " + str1 + " but was " + str2);
+        assertEquals(str2,str3, "error: expecting " + str2 + " but was " + str3);
+        assertEquals(str1, str4, "error: expecting " + str1 + " but was " + str4);
+
+        assertUniversalEquals(1, 2, "Err!"); // Err!: expecting 1(class java.lang.Integer) but got 2(class java.lang.Integer)
+        assertUniversalEquals("1", "2", "Err!"); // Err!: expecting 1(class java.lang.String) but got 2(class java.lang.String)
+        assertUniversalEquals("1", null, "Err!"); // Err!: expecting 1(class java.lang.String) but got null
+        assertUniversalEquals(1, "1", "Err!"); // Err!: expecting 1(class java.lang.Integer) but got 1(class java.lang.String)
+        assertUniversalEquals(null, null, "Err!"); // OK
+
+    }
 }
