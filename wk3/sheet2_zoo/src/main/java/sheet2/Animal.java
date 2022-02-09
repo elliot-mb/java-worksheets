1d1ba107469ac19874bdfedf91701c5b50202340
elliotmb <elliotbuckingham14@gmail.com>
2022-02-09 11:08:06 +0000

    eat your fruit

--- wk3/sheet2_zoo/src/main/java/sheet2/Animal.java
+++ wk3/sheet2_zoo/src/main/java/sheet2/Animal.java
@@ -5,5 +5,6 @@ public class Animal {
 	public String eat(Food food) {
 		return food.eaten(this);
 	}
+	public String eat(Fruit fruit) { return fruit.eaten(this); }
 
 }
--- wk3/sheet2_zoo/src/main/java/sheet2/Fruit.java
+++ wk3/sheet2_zoo/src/main/java/sheet2/Fruit.java
@@ -3,5 +3,12 @@ package sheet2;
 public class Fruit extends Food {
 
 	//TODO:
+    @Override
+    public String eaten(Animal animal) {
+        return "animal eats fruit";
+    }
+
+    @Override
+    public String eaten(Dog d) { return "dog eats food"; }
 
 }
