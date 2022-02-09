d209984455d04165b55c007acaa31882abaac46b
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-02-09 10:03:42 +0000

    Adding skeleton code

---
+++ wk3/TestUtils.java
@@ -0,0 +1,7 @@
+public class TestUtils {
+    static void assertEquals(String, String, String) {return null;}
+
+    static void assertEquals(int, int, String) {return null;}
+
+    public void assertUniversalEquals();
+}
