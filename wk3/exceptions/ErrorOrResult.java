b9033cf8df2611a9a988bce56c7d7db2c907b911
elliotmb <elliotbuckingham14@gmail.com>
2022-02-09 18:24:20 +0000

    error or result question push

--- wk3/exceptions/ErrorOrResult.java
+++ wk3/exceptions/ErrorOrResult.java
@@ -8,40 +8,40 @@ public class ErrorOrResult {
 
 // // // // DONT PUSH UNTIL MESSAGE JADE (WE'LL DECIDE WHICH VER TO USE
 
+    private int x;
+    private NumberFormatException e;
 
-//    ErrorOrResult(){
-//
-//    }
-//
-//    ErrorOrResult(){
-//
-//    }
-//
-//    static ErrorOrResult parseIntOrFail (String input){
-//        try{
-//            int x = Integer.parseInt(input);
-//        }catch (NumberFormatException e){
-//            error(e);
-//        }
-//        return
-//    }
-//    static boolean failed() {
-//
-//    }
-//
-//    static String error(NumberFormatException e) throws NoSuchElementException {
-//        if (failed()) return "error message";
-//        else throw new NoSuchElementException(e);
-//    }
-//
-//    static int result() throws NoSuchElementException {
-//
-//    }
-//
-//
-//    public static void main(String[] args) {
-//
-//    }
+    ErrorOrResult(int _x){
+        x = _x;
+    }
+
+    ErrorOrResult(NumberFormatException _e){
+        e = _e;
+    }
+
+    // only static method (used to make object)
+    static ErrorOrResult parseIntOrFail (String input){
+        try{
+            return new ErrorOrResult(Integer.parseInt(input));
+        }catch (NumberFormatException e){
+            return new ErrorOrResult(e);
+        }
+    }
+
+    public boolean failed() {
+        if(e == null) { return false; }
+        return true;
+    }
+
+    public String error() throws NoSuchElementException {
+        if (this.failed()) { return "NumberFormatException"; }
+        throw new NoSuchElementException(); // if there was no error, return a different error
+    }
+
+    public int result() throws NoSuchElementException {
+        if (!this.failed()) { return x; }
+        throw new NoSuchElementException(); //if no number
+    }
 }
 
     
