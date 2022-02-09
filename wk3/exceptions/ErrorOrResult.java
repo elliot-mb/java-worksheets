fc7272a2b3bb5b56b7976571846b81a51288cb5d
elliotmb <elliotbuckingham14@gmail.com>
2022-02-09 12:12:44 +0000

    exeptions

--- wk3/exceptions/ErrorOrResult.java
+++ wk3/exceptions/ErrorOrResult.java
@@ -1,7 +1,9 @@
+import java.util.NoSuchElementException;
+
 public class ErrorOrResult {
 
     boolean failed() {return false;}
-    String error() throws NoSuchElementException {}
+    String error() throws NoSuchElementException { return "s"; }
     int result() throws NoSuchElementException {return -1;}
 
     public static void main(String[] args) {
