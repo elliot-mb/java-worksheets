d9f5040094f653bde9e799b86330233b0ec003a4
elliotmb <elliotbuckingham14@gmail.com>
2022-02-16 11:41:42 +0000

    bek

--- wk4/BigExcinctKiwi.java
+++ wk4/BigExcinctKiwi.java
@@ -1,3 +1,9 @@
-public class BigExcinctKiwi {
+public class BigExcinctKiwi implements Bird {
+    private final Dodo d = new Dodo();
+    private final Kiwi k = new Kiwi();
 
+    public void fly(){
+        d.fly();
+        k.fly();
+    }
 }
