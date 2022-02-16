66adaa9733233d86d5938a838d6fb00728fadbb9
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-02-16 10:32:14 +0000

    Creating instances from EqualsGeneric class

--- wk4/QuestionRunner.java
+++ wk4/QuestionRunner.java
@@ -1,5 +1,8 @@
 public class QuestionRunner {
     public static void main(String[] args){
-        
+        System.out.println(EqualsGeneric.isEqual(1, 1)); //return true
+        System.out.println(EqualsGeneric.isEqual("1", 1)); //return false
+        System.out.println(EqualsGeneric.isEqual("Hello", "Hello")); //return true
+        System.out.println(EqualsGeneric.isEqual("Hello", "Hi")); //return false
     }
 }
