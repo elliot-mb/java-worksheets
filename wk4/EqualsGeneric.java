3f72320e15d3e889c1e5f1071eb9f9c83b46eade
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-02-16 10:36:53 +0000

    added static method reverse

--- wk4/EqualsGeneric.java
+++ wk4/EqualsGeneric.java
@@ -1,5 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class EqualsGeneric {
     static <T> boolean isEqual(T left, T right){
         return left.equals(right);
+    }
+
+    static List<T> reverse(List<T> list) {
+        List<T> reversedList = new ArrayList<T>();
+
+        int n = list.size();
+
+        int i = n;
+
+        while (i > -1) {
+            reversedList.add(list.get(i));
+        }
+
+        return reversedList;
     }
 }
