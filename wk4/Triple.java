94eec22703b9140994a75d5bacead0c93e67b011
elliotmb <elliotbuckingham14@gmail.com>
2022-02-16 10:56:46 +0000

    triple

---
+++ wk4/Triple.java
@@ -0,0 +1,8 @@
+public class Triple<T1, T2, T3> extends Tuple{
+    private final T3 c;
+
+    Triple(T1 a, T2 b, T3 c){
+        super(a, b);
+        this.c = c;
+    }
+}
--- wk4/Tuple.java
+++ wk4/Tuple.java
@@ -1,7 +1,7 @@
 public class Tuple<T1, T2> extends Id{
     private final T2 b;
 
-    <T1, T2> Tuple(T1 a, T2 b){
+    Tuple(T1 a, T2 b){
         super(a);
         this.b = b;
     }
