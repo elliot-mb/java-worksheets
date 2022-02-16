7843b51a6cd953298755ae78229979804abdb75b
elliotmb <elliotbuckingham14@gmail.com>
2022-02-16 11:33:41 +0000

    rot fixed

--- wk4/ROT13Formatter.java
+++ wk4/ROT13Formatter.java
@@ -1,11 +1,18 @@
 public class ROT13Formatter implements Formatter{
     //ROT13Formatter(){}
 
+    private final int rot = 13;
+
     public String format(String s){
         char[] chars = s.toCharArray();
         String result = "";
         for(char c : chars){
-            result += (char) (c + 13);
+            if(c > 64 && c < 91){
+                result += (char) (((c - 65 + this.rot) % 26) + 65);
+            }
+            else if(c > 96 && c < 123){
+                result += (char) (((c - 97 + this.rot) % 26) + 97);
+            }else{ result += c; }
         }
         return result;
     }
